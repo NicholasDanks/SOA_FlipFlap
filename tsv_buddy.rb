bd19d6e5c7bdc8912ed49e202a1f7146041c4f07
Nicholas Danks <nicholasdanks@hotmail.com>
2016-09-30 11:49:11 +0800

    Compled and tests passes

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -3,12 +3,27 @@ module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
-
+  	#separate tsv string into lines and fields
+    lines = tsv.split("\n")
+    header = lines.shift.split("\t")
+    @data = []
+    lines.each do |line|
+      values = line.split("\t")
+      record = values.map.with_index { |value, i| [header[i], value] }.to_h
+      @data << record
+    end
   end
-
+ 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
-
+    # Get the header
+    header = @data[1].keys.join("\t")
+    #Get the values
+    array = @data.map{|x| x.values.join("\t")}
+    #join into single string
+    contents = array.join("\n")
+    #join the header and values
+    final = header + "\n" + contents +"\n"
   end
 end
--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,1 +1,18 @@
 # Module that can be included (mixin) to take and output Yaml data
+module YamlBuddy
+  require 'yaml'
+  # take_yaml: converts a String with YAML data into @data
+  # parameter: yaml - a String in YAML format
+  
+  def take_yaml(yaml)
+  	#get yaml
+  	@data = YAML.load(yaml)
+  end
+ 
+  # to_yaml: converts @data into yml string
+  # returns: String in YML format
+  def to_yaml
+  @data.to_yaml
+  end
+end
+
